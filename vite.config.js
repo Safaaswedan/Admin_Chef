2ec6bc91220dbbc111440642118207d18af6e504
Safaaswedan <safasuedan@gmail.com>
2026-05-22 14:34:36 +0300

    Fix GitHub Pages configuration

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base:  './',   // <-- هذا السطر مهم جداً
+  base: '/Admin_Chef/',   // <-- هذا السطر مهم جداً
 })
