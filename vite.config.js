d602fefd2f7554b9564eea73caf00bc66bd11ebb
Safaaswedan <safasuedan@gmail.com>
2026-05-22 14:21:06 +0300

    Fix base path to relative './'

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Admin_Chef/',  // <-- هذا السطر مهم جداً
+  base:  './',   // <-- هذا السطر مهم جداً
 })
